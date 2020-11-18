bc59b923aaaad141e699996e10bc8205730ece4d
Arne Molland <arne@molland.sh>
2020-11-18 13:06:38 +0100

    Update tap source

--- essentials.sh
+++ essentials.sh
@@ -2,7 +2,7 @@
 
 xcode-select --install || true
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew tap caskroom/cask
+brew tap homebrew/cask
 brew cask install intune-company-portal
 brew cask install microsoft-office
 brew cask install microsoft-teams
